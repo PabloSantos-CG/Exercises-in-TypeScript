3a9f2302bcef8b7b3d2b302747437417f8c73738
PabloSantos-CG <comercial-pablo@outlook.com>
2023-11-06 11:20:10 -0300

    Update

---
+++ Exercicios/ex08/src/index.ts
@@ -0,0 +1,30 @@
+class Input {
+
+  static createdInput(type:string, id:number) {
+    const container = document.querySelector("#container")
+
+    const input = document.createElement("input")
+
+    input.type = type
+    input.id = `Tipo:${type}-${id}`
+
+    container.appendChild(input)
+    alert("Elemento criado :)")
+    return input
+  }
+
+  static init() {
+    const type = prompt("Vamos criar um input.\nInforme o tipo:")
+    const id = +prompt("Informe o id:")
+
+    if(type && id !== null) {
+      const result = Input.createdInput(type, id)
+      console.log(result)
+
+    } else {
+      alert("Sorry :( \nUnable to create element\nTry again :)")
+    }
+  }
+}
+
+document.querySelector("#init").addEventListener("click", Input.init)
