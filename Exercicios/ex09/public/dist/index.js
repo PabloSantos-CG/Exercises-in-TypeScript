b167239d74d05e437a40c61ef0718addba3234ff
PabloSantos-CG <comercial-pablo@outlook.com>
2023-11-09 10:21:05 -0300

    Update

--- Exercicios/ex09/public/dist/index.js
+++ Exercicios/ex09/public/dist/index.js
@@ -1,25 +1,3 @@
-class CreatePerson {
-    constructor(name, age, _job) {
-        this.name = name;
-        this.age = age;
-        this._job = _job;
-    }
-    verification() {
-        if (this.age > 18 && this._job === "SIM") {
-            this.addCar;
-        }
-    }
-    addCar(name, brand, year) {
-        if (this.age > 18 && this._job === "SIM") {
-            this.vehicule = [];
-            this.vehicule.push({ name, brand, year });
-        }
-        else {
-            this.vehicule = "Menor de idade";
-            console.log("Não será possível realizar a aquisição de um carro :(");
-            console.log(this);
-        }
-    }
-}
-const fulano = new CreatePerson("Guio", 26, "SIM");
-console.log({ fulano });
+const input = document.getElementById("task");
+const btnForm = document.getElementById("btnForm").value;
+console.log(input);
--- Exercicios/ex09/src/index.ts
+++ Exercicios/ex09/src/index.ts
@@ -1,32 +1,4 @@
-class CreatePerson {
-  public vehicule: object[] | "Menor de idade"
+const input = document.getElementById("task")
+const btnForm = document.getElementById("btnForm").value
 
-  constructor(
-    public name:string, 
-    public age:number, 
-    private _job:"SIM" | "NÃO",
-    ) {
-  }
-
-  verification() {
-    if(this.age > 18 && this._job === "SIM") {
-      this.addCar
-    }
-  }
-
-  addCar(name:string, brand:string, year:number) {
-    if(this.age > 18 && this._job === "SIM") {
-      this.vehicule = []
-      this.vehicule.push({ name, brand, year })
-
-    } else {
-      this.vehicule = "Menor de idade"
-      console.log("Não será possível realizar a aquisição de um carro :(")
-      console.log(this)
-    }
-  }
-}
-
-
-const fulano = new CreatePerson("Guio", 26, "SIM")
-console.log({ fulano })
+console.log(input)
