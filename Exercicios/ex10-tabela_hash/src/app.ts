2e70c6e9f227c7dc1e3557b23d4b833bab535e4e
PabloSantos-CG <comercial-pablo@outlook.com>
2024-01-25 18:49:08 -0300

    practicing

--- Exercicios/ex10-tabela_hash/src/app.ts
+++ Exercicios/ex10-tabela_hash/src/app.ts
@@ -1,10 +1,33 @@
-class hashTable {
+class HashTable {
   public table;
   public size: number;
 
   constructor() {
     this.table = new Array(123);
     this.size = 0;
+  }
+
+  public set(key: string, value: any) {
+    const index = this._hash(key);
+    this.table[index] = [key, value];
+    this.size++;
+  }
+
+  public get(key: string): string {
+    const index = this._hash(key);
+    return this.table[index];
+  }
+
+  public remove(key: string) {
+    const index = this._hash(key);
+
+    if (this.table[index] && this.table[index].length) {
+      this.table[index] = undefined;
+      this.size--;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   private _hash(key: string): number {
@@ -15,3 +38,10 @@ class hashTable {
     return hash % this.table.length;
   }
 }
+
+const newHash = new HashTable();
+newHash.set("maçã", 22);
+newHash.set("uva", 22);
+newHash.set("uvas", 23);
+console.log(newHash.table);
+console.log(newHash.get("uva"));
